e8ab86f1bcdaa8a218f42328773031374222fb42
max <max@localhost.localdomain>
2020-09-24 17:38:32 -0400

    mod

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          3000 Welcome to <a href="https://nextjs.org">Next.js!</a>
+          5000 Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
