736f6deea993b5358a5b7c2b2c501f265e2ee408
max <max@localhost.localdomain>
2020-09-24 17:53:14 -0400

    Updates

--- next/static/qFlrWjRIHS3d4ZA7BZNHx/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F1432fd92feb8437f91e3.css","static\u002Fchunks\u002Fpages\u002Findex-16296723d6ccf9f51d08.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-ed1b06dc12d6382f8fbd.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
