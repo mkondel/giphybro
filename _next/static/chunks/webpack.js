ee934b3ad8b0f3f685759fb89f1e291fcd78daeb
max <max@localhost.localdomain>
2020-09-24 22:08:39 -0400

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0ee5ec7559ee20e78fe7";
+/******/ 	var hotCurrentHash = "ff2cf6f5dc99828f4cba";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
